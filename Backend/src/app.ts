8e94c7363d4bc76d6f567aaa82d05894398912c0
Rimaru06 <shivraodanger04@gmail.com>
2024-02-13 14:52:46 +0530

    Add route for homepage

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,6 +8,9 @@ app.use(express.json());
 
 
 app.use('/user', UserRoutes);
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
 app.listen(PORT , ()=> {
     console.log("listening on port " + PORT)
 })
